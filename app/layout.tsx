7418409839a371f4921315cae55b35e2659020c3
rls-dnt <ramses.lopez@dentons.com>
2025-11-10 16:34:37 -0600

    feat: name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,45 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
+import './globals.css';
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const _geist = Geist({ subsets: ['latin'] });
+const _geistMono = Geist_Mono({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Bingo | Source Living',
+  description: 'Bingo app for Source Living events',
+  generator: 'EMPBI',
   icons: {
     icon: [
       {
         url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
+        media: '(prefers-color-scheme: light)'
       },
       {
         url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
+        media: '(prefers-color-scheme: dark)'
       },
       {
         url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
+        type: 'image/svg+xml'
+      }
     ],
-    apple: '/apple-icon.png',
-  },
-}
+    apple: '/apple-icon.png'
+  }
+};
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`font-sans antialiased`}>
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
