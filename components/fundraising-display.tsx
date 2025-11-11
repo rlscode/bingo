b8eb62a551e3ef177d7682b0fee75b9e5a3e7996
Ramses Lopez <ramses.lopez@outlook.com>
2025-11-11 16:06:47 -0600

    Update fundraising-display.tsx

--- components/fundraising-display.tsx
+++ components/fundraising-display.tsx
@@ -10,7 +10,7 @@ export function FundraisingDisplay() {
   const totalCards = 150;
 
   useEffect(() => {
-    const finalAmount = 32000;
+    const finalAmount = 36000;
     const duration = 2000;
     const steps = 60;
     const increment = finalAmount / steps;
